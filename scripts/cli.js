731f602f16586a61a7142db3c08075138c00ee68
David Luna <david.luna@contentsquare.com>
2023-01-16 16:49:23 +0100

    chore: add helper script

---
+++ scripts/cli.js
@@ -0,0 +1,48 @@
+const { execSync } = require('child_process');
+
+// Get params
+const [nodePath, cliPath, ...params] = process.argv;
+const rootPath = cliPath.replace('/scripts/cli.js', '');
+
+
+const buildAssets = {
+  angular: `${rootPath}/packages/app-react/dist`,
+  react: `${rootPath}/packages/app-react/build`,
+  vue: `${rootPath}/packages/app-react/build`,
+};
+const wrapperSrc = {
+  capacitor: `${rootPath}/packages/capacitor/dist`,
+  cordova: `${rootPath}/packages/cordova/www`,
+};
+
+/**
+ * 
+ * @param {string} framework the UI framework to build (angular, react, vue)
+ * @param {string} wrapper the wrapper to use (capacitor, cordova)
+ * @param {string} platform the platform to run (android, ios)
+ */
+function prepare(framework, wrapper, /*platform*/) {
+  // const wrapperBin = wrapper === 'capacitor' ? 'npx cap' : 'cordova' 
+  const source = buildAssets[framework];
+  const dest = wrapperSrc[wrapper];
+
+  if (!source) {
+    console.error(`UI framework ${framework} unknown. Available ones are ${Object.keys(buildAssets)}`);
+    process.exit(-1);
+  }
+  if (!dest) {
+    console.error(`Native wrapper ${wrapper} unknown. Available ones are ${Object.keys(wrapperSrc)}`);
+    process.exit(-1);
+  }
+
+  execSync('npm run build', { cwd: `${rootPath}/packages/app-${framework}` });
+  execSync(`cp ${source}/* ${dest}`);
+  // execSync(`${wrapperBin} run ${platform}`, { cwd: `${rootPath}/packages/${wrapper}` });
+}
+
+console.log(rootPath, process.cwd());
+
+// Main process
+const [framework, wrapper, platform] = rest;
+
+prepare(framework, wrapper);
