fc4d13a26634cab0782b8947db9b871f8ba42e93
David Luna <david.luna@contentsquare.com>
2023-01-20 12:46:16 +0100

    chore: add hooks

---
+++ scripts/hooks.js
@@ -0,0 +1,35 @@
+const { execSync } = require('child_process');
+
+function preCommit(cwd) {
+  const output = execSync('git status', { cwd }).toString();
+  const changes = output.split('\n').filter((line) => {
+    return line.includes('packges/cordova') || line.includes('packages/capacitor');
+  });
+
+  if (changes.length) {
+    console.error([
+      'Your commit has changes to the native projects!!!',
+      '  - revert changes or additions into capacitor or cordova projects and commit',
+      '  - if you wish to proceed use the --no-verify flag to your commit command',
+    ].join('\n'));
+    process.exit(-1);
+  }
+}
+
+
+// Get params
+const [nodePath, cliPath, sh, hook] = process.argv;
+const rootPath = cliPath.replace('/scripts/hooks.js', '');
+
+// Main process
+const hooks = { preCommit };
+const hookFn = hooks[hook];
+
+if (!hookFn) {
+  console.error(`CLI hook ${hook} does not exist.`)
+  process.exit(-1);
+}
+
+// Run the action
+console.log(`Running hook ${hook}`)
+hookFn.apply(null, [rootPath]);
