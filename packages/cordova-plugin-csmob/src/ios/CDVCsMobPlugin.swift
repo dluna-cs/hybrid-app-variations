10119a5349e594abb8917f5b23f3b222a7a4582f
David Luna <david.luna@contentsquare.com>
2023-01-24 07:58:58 +0100

    fix: fix typing in registered commands dict

--- packages/cordova-plugin-csmob/src/ios/CDVCsMobPlugin.swift
+++ packages/cordova-plugin-csmob/src/ios/CDVCsMobPlugin.swift
@@ -7,25 +7,23 @@ import WebKit
 @objc(CDVCsMobPlugin) class CDVCsMobPlugin : CDVPlugin {
     
     var registeredCommands = [
-        "optin": [CommandOptIn()],
-        "optout": [CommandOptOut()],
-        "sendScreenName": [CommandSendScreenName()],
-        "handleUrl": [CommandHandleUrl()],
-        "sendTransaction": [CommandSendTransaction()],
-        "sendDynamicVar": [CommandSendDynamicVar()],
-        "handleUrl": [CommandHandleUrl()]
-    ]
+        "optin": CommandOptIn(),
+        "optout": CommandOptOut(),
+        "sendScreenName": CommandSendScreenName(),
+        "handleUrl": CommandHandleUrl(),
+        "sendTransaction": CommandSendTransaction(),
+        "sendDynamicVar": CommandSendDynamicVar(),
+        "handleUrl": CommandHandleUrl()
+    ] as [String : CommandHandlerProtocol]
 
   @objc(sendCommand:) // Declare your function name.
   func sendCommand(command: CDVInvokedUrlCommand) { // write the function code.
       
     let name = command.arguments[0] as? String ?? ""
     
-    if let commandHandlers = registeredCommands[name] {
-      // TODO: wait for all executions or fire and forget
-      for handler in commandHandlers {
-          handler.handleCommand(command: command)
-      }
+      if let commandHandler = registeredCommands[name] {
+      // TODO: wait for execution or fire and forget ???
+      commandHandler.handleCommand(command: command)
     } else {
       print("Unknown command \(name)")
     }
