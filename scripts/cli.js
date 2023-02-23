6410bcbabba83ba873705dfc7b66f194b1139bd8
David Luna <david.luna@contentsquare.com>
2023-02-23 14:21:14 +0100

    chore: fix assets path to pokemon app

--- scripts/cli.js
+++ scripts/cli.js
@@ -6,7 +6,7 @@ const rootPath = cliPath.replace('/scripts/cli.js', '');
 
 
 const buildAssets = {
-  pokemon: `${rootPath}/packages/app-angular/dist/PokeApp`,
+  pokemon: `${rootPath}/packages/app-pokemon/dist/PokeApp`,
   angular: `${rootPath}/packages/app-angular/dist/app-angular`,
   react: `${rootPath}/packages/app-react/build`,
   // vue: `${rootPath}/packages/app-react/build`,
